9803f65c820d34d8fc635cfcf469f0ac4779cc7c
1334 <1334@shadowproject.net>
2018-10-03 15:01:23 +0200

    implement basic dijkstra algorithm

--- index.js
+++ index.js
@@ -2,32 +2,95 @@
 
 const graph = {
   a: {
-    edges: {
-      b: 10,
-      c: 20
-    },
+    value: 'a',
+    edges: { b: 10, c: 20 },
     minWeight: Infinity
   },
   b: {
-    d: 50,
-    e: 10
+    value: 'b',
+    edges: { d: 50, e: 10 },
+    minWeight: Infinity
   },
   c: {
-    d: 20,
-    e: 33
+    value: 'c',
+    edges: { d: 20, e: 33 },
+    minWeight: Infinity
   },
   d: {
-    e: 20,
-    f: 2
+    value: 'd',
+    edges: { e: 20, f: 2 },
+    minWeight: Infinity
   },
   e: {
-    f: 1
+    value: 'e',
+    edges: { f: 1 },
+    minWeight: Infinity
+  },
+  f: {
+    value: 'f',
+    edges: {},
+    minWeight: Infinity
   }
 }
 
-function dijkstra(graph) {
+function dijkstra(graph, root) {
+  const queue = new Queue();
+  const result = {
+    weigths: []
+  };
+  let currentNode;
 
+  root.minWeight = 0;
+  queue.insert(root);
+  currentNode = queue.next();
+
+  while(currentNode) {
+    for (let key in currentNode.edges) {
+      if (currentNode.edges[key] + currentNode.minWeight <= graph[key].minWeight) {
+        graph[key].minWeight = currentNode.edges[key] + currentNode.minWeight;
+        graph[key].previous = currentNode;
+      }
+      queue.insert(graph[key]);
+    }
+    currentNode = queue.next();
+  }
+
+  for (let node in graph) {
+    result.weigths.push(graph[node].minWeight);
+  }
+  // console.log(graph);
+
+  return result.weigths;
 }
 
-console.log(dijkstra(graph));
-console.assert(dijkstra(graph), [0, 10, 20, 40, 20, 21]);
+function Queue() {
+  this.storage = [];
+}
+
+Queue.prototype.insert = function (element) {
+  if (!this.storage.includes(element)) this.storage.push(element);
+  this.storage.sort((a, b) => a.minWeight - b.minWeight);
+};
+
+Queue.prototype.next = function () {
+  if (this.isEmpty()) return undefined;
+  return this.storage.shift();
+};
+
+Queue.prototype.isEmpty = function () {
+  return !this.storage.length;
+};
+
+console.log(dijkstra('weights', graph, graph.a));
+// console.assert(dijkstra(graph, graph.a), [0, 10, 20, 40, 20, 21]);
+
+// const queue = new Queue();
+// let nodeb = Object.assign({}, graph.b, {minWeight: 10});
+// let nodec = Object.assign({}, graph.c, {minWeight: 20});
+// console.log('isEmpty:', queue.isEmpty());
+// queue.insert(nodec);
+// queue.insert(nodeb);
+// console.log('isEmpty:', queue.isEmpty());
+
+// console.assert(queue.storage[0] === nodeb);
+// console.assert(queue.storage[1] === nodec);
